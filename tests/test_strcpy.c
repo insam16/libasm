5fdc46d0536e4b5e1dc9cfba990b22be9b0fce80
insam16 <yumin5541@naver.com>
2026-07-04 11:19:36 +0900

    Update: test_strcpy.c에 fork() SEGFAULT tests 추가

--- tests/test_strcpy.c
+++ tests/test_strcpy.c
@@ -1,15 +1,19 @@
 // test_strcpy.c
 
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/wait.h>
 
 extern char *ft_strcpy(char *dest, const char *src);
 
+int test_null_case(char *func_name, char *func(char *, const char *), char *dest, char *src);
+
 int main(void)
 {
-	char *src[] = {"", "a", "hello", "42gyeongsan", "yuna", NULL};
-	char dest_mine[10];
-	char dest_libc[10];
+	char *src[] = {"", "a", "hello", "42gyeongsan", "yuna"};
+	char dest_mine[12];
+	char dest_libc[12];
 	int fail = 0;
 
 	char *mine;
@@ -19,7 +23,6 @@ int main(void)
 	for (; i < 5; ++i) {
 		mine = ft_strcpy(dest_mine, src[i]);	
 		libc = strcpy(dest_libc, src[i]);
-		if (i == 3) printf("src[%d] is UB. buffer size missing\n", i);
 		printf("src: %s\n", src[i]);
 		printf("dest_mine: %s, mine: %s\n", dest_mine, mine);
 		printf("dest_libc: %s, libc: %s\n", dest_libc, libc);
@@ -28,23 +31,44 @@ int main(void)
 		if (strcmp(dest_mine, dest_libc)) fail = 1;
 	}
 
-	//TODO: fork() SEGFAULT tests
-	mine = ft_strcpy(NULL, src[i-1]);	
-	libc = strcpy(NULL, src[i-1]);
-	printf("src: %s\n", src[i]);
-	printf("dest_mine: %s\n, mine: %s", dest_mine, mine);
-	printf("dest_libc: %s\n, libc: %s", dest_libc, libc);
-	printf(strcmp(dest_mine, dest_libc) ? "FAIL" : "OK");
-	printf("\n\n");
-	if (strcmp(dest_mine, dest_libc)) fail = 1;
+	//fork() SEGFAULT tests
 
-	mine = ft_strcpy(NULL, NULL);	
-	libc = strcpy(NULL, NULL);
-	printf("src: %s\n", src[i]);
-	printf("dest_mine: %s\n, mine: %s", dest_mine, mine);
-	printf("dest_libc: %s\n, libc: %s", dest_libc, libc);
-	printf(strcmp(dest_mine, dest_libc) ? "FAIL" : "OK");
+	//(NULL, "yuna")
+	printf("(NULL, \"yuna\")\n");
+	test_null_case("ft_strcpy", ft_strcpy, NULL, "yuna");
+	test_null_case("strcpy", strcpy, NULL, "yuna");
 	printf("\n\n");
-	if (strcmp(dest_mine, dest_libc)) fail = 1;
+
+	//(dest, NULL)
+	printf("(dest, NULL)\n");
+	test_null_case("ft_strcpy", ft_strcpy, dest_mine, NULL);
+	test_null_case("strcpy", strcpy, dest_libc, NULL);
+	printf("\n\n");
+
+	//(NULL, NULL)
+	printf("(NULL, NULL)\n");
+	test_null_case("ft_strcpy", ft_strcpy, NULL, NULL);
+	test_null_case("strcpy", strcpy, NULL, NULL);
+	printf("\n\n");
+
 	return (fail);
+}
+
+int test_null_case(char *func_name, char *func(char *, const char *), char *dest, char *src)
+{
+	pid_t pid = fork();
+
+	if (pid == 0) {
+		func(dest, src);
+		_exit(0);
+	} else {
+		int status;
+		waitpid(pid, &status, 0);
+		if (WIFSIGNALED(status)) {
+			printf("%s: 자식이 시그널 %d로 종료됨 (SIGSEGV==%d)\n",
+				func_name, WTERMSIG(status), SIGSEGV);
+			return 1; // 세그폴트
+		}
+		return 0;
+	}
 }
